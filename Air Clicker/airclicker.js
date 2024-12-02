cc6168c26065eb8f19b51c39865db5ded2679208
bananaman-code <siscool.7777@gmail.com>
2024-12-02 17:58:03 -0500

    Update airclicker.js

--- Air Clicker/airclicker.js
+++ Air Clicker/airclicker.js
@@ -1,12 +1,30 @@
 var breath = 25;
 var air = 0;
+var AirPerSecond = 0;
+var breathPerSecond = 0.2;
+var airPerClick = 1;
+
 $(".maincloud").click(()=> {
-  air++;
-  $(".howmuchair").html(air + ' air ');
+  air = airPerClick + air
+  showingAir = Math.round(air*10) / 10
+  $(".howmuchair").html(showingAir + ' air ');
 });
 
 $(".personBreathing ").click(()=>{
-   breath += breath;
-  $('#airCost').text(breath + ' air ');
-  console.log('click');
+  if (air >= Math.round(breath)) {
+    air = air - Math.round(breath)
+    AirPerSecond = AirPerSecond + breathPerSecond
+    showingAir = Math.round(air*10) / 10
+    $(".howmuchair").html(showingAir + ' air ');
+    breath = breath * 1.1
+   $('#airCost').text(Math.round(breath) + ' air ');
+   console.log('click');
+  } 
 });
+
+
+setInterval(function() {
+  air=AirPerSecond+air
+  showingAir = Math.round(air*10) / 10
+  $(".howmuchair").html(showingAir + ' air ');
+},1000)
